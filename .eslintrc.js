53395cf4aa7a8fd6c62bcfb5da285e9258df6d7b
Can Serkan UREN <canserkan.uren@gmail.com>
2020-10-17 14:45:25 +0200

    feat: update eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module'
+    project: './tsconfig.json'
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'jest'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
+    'plugin:jest/recommended',
+    'plugin:jest/style'
   ],
   root: true,
   env: {
@@ -19,6 +20,34 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, 'always', { omitLastInOneLineBlock: true }],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest/no-test-return-statement': 'error',
+    'jest/no-alias-methods': 'error',
+    'jest/no-jasmine-globals': 'error',
+    'jest/expect-expect': [
+      'error',
+      {
+        assertFunctionNames: [
+          'expect',
+          'assertContentIsEqualTo',
+          'expectStatutIsCorrectlyDisplayed',
+          'assertContentIsHidden',
+          'assertContentIsDisplayed'
+        ]
+      }
+    ],
+    'jest/no-test-prefixes': 'error',
+    '@typescript-eslint/no-parameter-properties': [
+      2,
+      {
+        allows: ['private', 'public']
+      }
+    ]
   }
 };
