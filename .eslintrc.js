13a9a3647e11e64ae78a96234cacbda13e2c517b
canserkanuren <canserkan.uren@gmail.com>
2020-10-21 13:00:08 +0200

    feat: add javascript guideline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  plugins: ['@typescript-eslint/eslint-plugin', 'prettier', 'jest'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'plugin:jest/recommended',
-    'plugin:jest/style'
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true
-  },
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'prettier'],
+  env: { browser: true, node: true, jest: true },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    semi: [2, 'always', { omitLastInOneLineBlock: true }],
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-    'jest/no-test-return-statement': 'error',
-    'jest/no-alias-methods': 'error',
-    'jest/no-jasmine-globals': 'error',
-    'jest/expect-expect': [
-      'error',
-      {
-        assertFunctionNames: [
-          'expect',
-          'assertContentIsEqualTo',
-          'expectStatutIsCorrectlyDisplayed',
-          'assertContentIsHidden',
-          'assertContentIsDisplayed'
-        ]
-      }
-    ],
-    'jest/no-test-prefixes': 'error',
-    '@typescript-eslint/no-parameter-properties': [
-      2,
-      {
-        allows: ['private', 'public']
-      }
-    ]
+    'prettier/prettier': 'error'
   }
 };
